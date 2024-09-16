1a696207d0a749a0db7af34c627d214e570a281f
Ismail-Taha Allali <isallali@e1r2p9.1337.ma>
2024-09-16 16:46:41 +0100

    70% success

---
+++ C05/ex03/ft_recursive_power.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isallali <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/07 22:38:25 by isallali          #+#    #+#             */
+/*   Updated: 2024/09/07 22:43:36 by isallali         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	if (power > 1)
+		nb = nb * ft_recursive_power(nb, power - 1);
+	return (nb);
+}
---
+++ C05/ex07/ft_find_next_prime.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isallali <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/08 15:16:14 by isallali          #+#    #+#             */
+/*   Updated: 2024/09/08 16:39:21 by isallali         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_is_prime(int nb)
+{
+	int	n;
+
+	n = 2;
+	if (nb <= 1)
+		return (0);
+	while (n <= nb / n)
+	{
+		if (nb % n == 0)
+			return (0);
+		n++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	int	n;
+
+	n = nb;
+	if (nb <= 1)
+		return (2);
+	while (!ft_is_prime(n))
+		n++;
+	return (n);
+}
